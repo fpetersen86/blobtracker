4ff211a1b8209b438469b820a62ecbcf9e07381b
Florian Petersen <fpetersen@fnord.honigtopf>
2013-06-10 21:35:39 +0200

    initial kernel implementation for lens correction

--- camarray.cu
+++ camarray.cu
@@ -1,8 +1,55 @@
 #include "camarray.h"
 #include <stdio.h>
 
+
+
+//GPU Kernel
+__global__ void findBlobs(char *image, float *output, int width, int height, float strength, float zoom)
+{
+	int y = blockIdx.y * blockDim.y + threadIdx.y;         // coordinates within 2d array follow from block index and thread index within block
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int elemID = y*width + x;                              // index within linear array
+
+	// compute cells needed for update (neighbors + central element)
+	int borderFlag = (x > 0);                              // boolean values enable border handling without thread divergence
+	float leftNeighb = input[elemID - borderFlag];
+	borderFlag = (x < (width - 1));
+	float rightNeighb = input[elemID + borderFlag];
+	borderFlag = -(y > 0);									// unary minus turns boolean value into boolean bitwise mask
+	float topNeighb = input[elemID - (borderFlag & width)];	
+	borderFlag = -(y < (height - 1));
+	float bottomNeighb = input[elemID + (borderFlag & width)];
+	float currElement = input[elemID];
+	
+	int halfWidth = width / 2;
+	int halfHeight = height / 2;
+	float correctionRadius = sqrtf(width * width + height * height) / strength;
+	int newX = x - halfWidth;
+	int newY = y - halfHeight;
+
+	float distance = sqrtf(newX * newX + newY * newY);
+	float r = distance / correctionRadius;
+	
+	float theta = 1;
+	if(r != 0)
+	{
+		theta = atan(r)/r;
+	}
+	
+	float sourceX = halfWidth + theta * newX * zoom;
+	float sourceY = halfHeight + theta * newY * zoom;
+	
+	output[elemID] = image[sourceY*width + sourceX];
+	
+	//output[elemID] = currElement + mu * ( (leftNeighb-currElement) + (rightNeighb-currElement) + (bottomNeighb-currElement) + (topNeighb-currElement) );
+}
+
+
+
 CamArray::CamArray(webcamtest* p)
 {
+	int xSize = 320;
+	int ySize = 240;
 	
 	//initialise cams
 	QDir d("/dev/");
@@ -15,7 +62,7 @@ CamArray::CamArray(webcamtest* p)
 	sem->acquire(numCams);
 	
 	
-	int bufferSize = 320 * 240 * numCams * sizeof(char);
+	int bufferSize = xSize * ySize * numCams * sizeof(char);
 	//host buffers
 	char* h_a = (char *) malloc(bufferSize);
 	
@@ -53,4 +100,3 @@ CamArray::~CamArray()
 		delete cams[i];
 
 }
-
